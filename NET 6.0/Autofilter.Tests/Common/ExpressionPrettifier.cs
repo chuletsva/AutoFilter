974c3b5102735fad0f923d8d281ebf1dc3a6b3e2
Vyacheslav Chulets <chuletsva@gmail.com>
2023-01-25 17:11:38 +0200

    small refactoring

--- NET 6.0/Autofilter.Tests/Common/ExpressionPrettifier.cs
+++ NET 6.0/Autofilter.Tests/Common/ExpressionPrettifier.cs
@@ -9,11 +9,10 @@ internal static class ExpressionPrettifier
     {
         string lambda = lambdaExpr.ToString();
 
-        lambda = Regex.Replace(lambda, @"(?'prop'x.Prop)(?'num'\d+)", match => match.Groups["num"].Value);
+        lambda = Regex.Replace(lambda, @"x.Prop(?'num'\d+)", match => match.Groups["num"].Value);
         lambda = Regex.Replace(lambda, @"OrElse", _ => "or");
         lambda = Regex.Replace(lambda, @"AndAlso", _ => "and");
         lambda = Regex.Replace(lambda, @"x => ", _ => "");
-        lambda = Regex.Replace(lambda, @"$((.+))^", _ => "");
         lambda = Regex.Replace(lambda, @"^\((?'content'.+)\)$", match => match.Groups["content"].Value);
 
         return lambda;
