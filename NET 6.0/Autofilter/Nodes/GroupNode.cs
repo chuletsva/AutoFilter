8cf02bef0d20e3c2de15596dbeb6cb636046ec7e
Vyacheslav Chulets <chuletsva@gmail.com>
2023-01-25 22:07:13 +0200

    fix typo

--- NET 6.0/Autofilter/Nodes/GroupNode.cs
+++ NET 6.0/Autofilter/Nodes/GroupNode.cs
@@ -16,7 +16,7 @@ internal class GroupNode : INode
 
     public Expression BuildExpression()
     {
-        Expression rightOperand = _children[0].BuildExpression();
+        Expression leftOperand = _children[0].BuildExpression();
 
         for (int i = 1; i < _children.Count; i++)
         {
@@ -25,23 +25,23 @@ internal class GroupNode : INode
                 case LogicOperator.Or:
                     int j = i;
 
-                    Expression leftOperand = _children[i].BuildExpression();
+                    Expression rightOperand = _children[i].BuildExpression();
 
                     while (j + 1 < _children.Count && _children[j + 1].Operator == LogicOperator.And)
-                        leftOperand = Expression.AndAlso(leftOperand, _children[++j].BuildExpression());
+                        rightOperand = Expression.AndAlso(rightOperand, _children[++j].BuildExpression());
 
-                    rightOperand = Expression.OrElse(rightOperand, leftOperand);
+                    leftOperand = Expression.OrElse(leftOperand, rightOperand);
 
                     i = j;
                     break;
                 case LogicOperator.And:
-                    rightOperand = Expression.AndAlso(rightOperand, _children[i].BuildExpression());
+                    leftOperand = Expression.AndAlso(leftOperand, _children[i].BuildExpression());
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
         }
 
-        return rightOperand;
+        return leftOperand;
     }
 }
